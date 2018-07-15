b8d83553c9445443f5370c2fb50b003b2b4a4c0d
sribigdata <39053877+sribigdata@users.noreply.github.com>
2018-07-15 19:47:01 +0100

    Create ListOfItem.scala

---
+++ ListOfItem.scala
@@ -0,0 +1,12 @@
+under project strcture > src > main > scala >   
+
+
+/* List of Items */
+case class ListOfItem(val price: BigDecimal, val name: String)
+
+object ListOfItem {
+
+  /*def Cola(): ListOfItem = {
+    ListOfItem(0.50, "Cola")
+  }*/
+}
